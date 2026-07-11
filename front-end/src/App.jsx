3a5f32411da1747841095307ad521303e32c93c9
vijay404914 <vijaysahu4914@gmail.com>
2026-07-11 15:28:25 +0530

    tailwind configuration

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind CSS v4 🚀
+      </h1>
+    </div>
   )
 }
 
